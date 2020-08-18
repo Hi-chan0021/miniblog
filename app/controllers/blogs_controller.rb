b0e2b95b147c753fb57a622cb0d9e67ad41fe8de
EC2 Default User <ec2-user@ip-172-31-9-26.ap-northeast-1.compute.internal>
2020-08-18 23:56:21 +0000

    ブロク投稿のコーディング

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,19 @@
 class BlogsController < ApplicationController
   
   def index
+    @blogs = Blog.all
+  end
+  
+  def new
+  end
+  
+  def create
+    Blog.create(blog_params)
+  end
+  
+  private
+  def blog_params
+    params.permit(:title, :text)
   end
   
 end

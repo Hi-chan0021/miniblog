116f8f77279dd8608f44c6fc8c32ab226f254b9f
EC2 Default User <ec2-user@ip-172-31-9-26.ap-northeast-1.compute.internal>
2020-08-19 07:28:20 +0000

    コメント欄の追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'blogs#index'
   
-  get 'blogs' => 'blogs#index'
-  get 'blogs/new' => 'blogs#new'
-  get 'blogs/:id' => 'blogs#show'
-  post 'blogs' => 'blogs#create'
-  delete 'blogs/:id' => 'blogs#destroy'
-  get 'blogs/:id/edit' => 'blogs#edit'
-  patch 'blogs/:id' => 'blogs#update'
+  resources :blogs do
+    resources :comments, only: [:create]
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

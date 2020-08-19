3db45557e5e04bfd3bae99d996fb4e4dfc7543a0
EC2 Default User <ec2-user@ip-172-31-9-26.ap-northeast-1.compute.internal>
2020-08-19 00:46:01 +0000

    ログイン画面の実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,7 @@
 class BlogsController < ApplicationController
   
+  before_action :move_to_index, except: :index
+  
   def index
     @blogs = Blog.all
   end
@@ -14,6 +16,10 @@ class BlogsController < ApplicationController
   private
   def blog_params
     params.permit(:title, :text)
+  end
+  
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
   
 end
